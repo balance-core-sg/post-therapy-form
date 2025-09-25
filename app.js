116a01ab14ad1ae30b7baece725d9bf961f980f5
balance-core-sg <balancecoren8n@gmail.com>
2025-09-25 16:40:44 +0700

    init

---
+++ app.js
@@ -0,0 +1,37 @@
+(function(){
+  const workflowUrl = 'https://balancecore.n8n.superlazy.ai/webhook';
+
+  fetch(`${workflowUrl}/clients`)
+    .then(response => response.json())
+    .then(({ data: clients }) => {
+      const clientOptions = clients.map(client => `<option value="${client['Phone Number']}">${client.Name}</option>`)
+      document.getElementById('client').innerHTML += clientOptions.join('');
+    });
+
+  document.querySelector('button[name="submit"]').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const formData = new FormData;
+    formData.append('client', document.getElementById('client').value);
+    formData.append('key_points', document.getElementById('key_points').value || '');
+    formData.append('homework', document.getElementById('homework').value || '');
+
+    const attachments = document.getElementById('file').files;
+    for(let i = 0; i < attachments.length; i++) {
+      const attachment = attachments[i];
+      formData.append(`file[]`, attachment);
+    }
+
+    this.textContent = 'Please wait...'
+    this.setAttribute('disabled', 'disabled');
+
+    fetch(`${workflowUrl}-test/post-theraphy`, {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.error);
+  });
+
+})();
