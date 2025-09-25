1deeabd9f23ea8ad1ce19b8197800df262defc24
balance-core-sg <balancecoren8n@gmail.com>
2025-09-26 06:27:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,47 @@
       document.getElementById('client').innerHTML += clientOptions.join('');
     });
 
+  const validateForm = (data = {}) => {
+    const schema = joi.object({
+      clientPhone: joi.string().required(),
+      clientName: joi.string().required(),
+      keyPoints: joi.string().allow('', null),
+      homework: joi.string().allow('', null),
+    });
+
+    return schema.validate(data, { abortEarly: false }); // show all errors
+  }
+
   document.querySelector('button[name="submit"]').addEventListener('click', function(e){
     e.preventDefault();
+    const clientPhone = document.getElementById('client').value;
+    const clientName = document.querySelector(`option[value="${clientPhone}"]`).text;
+    const keyPoints = document.getElementById('key_points').value || '';
+    const homework = document.getElementById('homework').value || '';
+
+    const errors = validateForm({
+      clientPhone,
+      clientName,
+      keyPoints,
+      homework
+    });
+
+    if (errors?.error?.details) {
+      const errorMessage = errors
+        .error
+        .details
+        .map(error => error.message)
+        .join('\n');
+
+      alert(errorMessage);
+      return;
+    }
 
     const formData = new FormData;
-    formData.append('client', document.getElementById('client').value);
-    formData.append('key_points', document.getElementById('key_points').value || '');
-    formData.append('homework', document.getElementById('homework').value || '');
+    formData.append('client_name', clientName);
+    formData.append('client_phone', clientPhone);
+    formData.append('key_points', keyPoints);
+    formData.append('homework', homework);
 
     const attachments = document.getElementById('file').files;
     for(let i = 0; i < attachments.length; i++) {
@@ -32,11 +66,11 @@
     .then(response => response.json())
     .then(response => {
       this.textContent = 'Done'
-      this.setAttribute('disabled', 'disabled');
     })
-    .catch(console.error);
+    .catch((error) => {
+      this.textContent = 'Error'
+      console.error(error);
+    });
   });
 
-
 })();
-
